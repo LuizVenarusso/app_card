e610e8f21cf6d6bd8cbe82958cc8118aeaac735e
Luiz Venarusso <luiz-gustavo10@hotmail.com>
2018-07-28 15:48:00 -0300

    Usuario faz upload de um arquivo csv

--- spec/features/user_visit_homepage_spec.rb
+++ spec/features/user_visit_homepage_spec.rb
@@ -6,12 +6,17 @@ feature 'User visit homepage' do
     visit root_path
 
     expect(page).to have_content('Aplicativo de Cartões de Alimentação')
+    expect(page).to have_content('Importar o dado')
   end
 
   scenario 'failed' do
 
     visit root_path
 
+    expect(page).not_to have_content('Nome Completo')
+    expect(page).not_to have_content('Email')
+    expect(page).not_to have_content('Token')
+    expect(page).not_to have_content('Data de Admissão')
     expect(page).not_to have_content('Valor disponível no cartão')
   end
 end
